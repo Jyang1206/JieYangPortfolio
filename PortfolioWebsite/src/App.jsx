5b4f12b51eaaae8d5c7f07099e9d1c744cd46442
Jie Yang <ngjieyang@hotmail.com>
2025-09-29 12:45:26 +0800

    Add ThemeToggle

--- PortfolioWebsite/src/App.jsx
+++ PortfolioWebsite/src/App.jsx
@@ -1,18 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home }from "./Pages/Home";
-import { NotFound }from "./Pages/NotFound";
+import { Home } from "./Pages/Home";
+import { NotFound } from "./Pages/NotFound";
+
 function App() {
     return (
-        <>
- <BrowserRouter>
-     <Routes>
-         <Route index element = {<Home />}>
-         <Route path = "*" element={<NotFound />}>
-     </Routes>
- </Browser Router>
- <>
- );
-
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Home />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        );
 }
 
 export default App
